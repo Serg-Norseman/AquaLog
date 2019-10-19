be0d919c77d5be27d320d0b130093bc8aac5d8ad
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2019-10-19 22:30:10 +0300

    Minor improvement

--- build_mswin_x86.cmd
+++ build_mswin_x86.cmd
@@ -1,6 +1,6 @@
 @echo off
 set MSBDIR=@%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-%MSBDIR%\msbuild.exe AquaLog.sln /p:Configuration="Debug" /p:Platform="Any CPU" /t:Rebuild /p:TargetFrameworkVersion=v4.5
+%MSBDIR%\msbuild.exe AquaLog.sln /p:Configuration="Debug" /p:Platform="x86" /t:Rebuild /p:TargetFrameworkVersion=v4.5
 
 set BUILD_STATUS=%ERRORLEVEL% 
 if %BUILD_STATUS%==0 goto test 
